89d1f2f595445543b46036311391b0e1065b6b03
Senyang Jiang <senyangjiang@yahoo.com>
2019-04-15 22:09:59 -0700

    lab2

--- Lab2/poornames.sh
+++ Lab2/poornames.sh
@@ -6,7 +6,7 @@ then
     path="$1"
 fi
 
-pathcheck=`echo $path | grep -E '^[^-]'`
+pathcheck=`echo "$path" | grep -E '^[^-]'`
 if [ ! -z "$2" ]
 then
     echo 'ERROR: too many arguments' 1>&2
@@ -21,7 +21,7 @@ then
     exit 1
 fi
 
-lastcomp=`echo $path | grep -E '/[A-za-z_]{1}[A-za-z._-]{,13}$'`
+lastcomp=`echo "$path" | grep -E '/[[:alpha:]_]{1}[[:alpha:]._-]{,13}$'`
 if [ -z "$lastcomp" ]
 then
     echo "$path"
@@ -30,25 +30,23 @@ fi
 
 findpoor()
 {
-    files=`ls $1`
-    dupl=`ls $1 | grep -E '^[A-za-z_]{1}[A-za-z._-]{,13}$' |
+    dupl=`ls "$1" | grep -E '^[[:alpha:]_]{1}[[:alpha:]._-]{,13}$' |
  tr '[:upper:]' '[:lower:]' | sort | uniq -d`
     for s in $dupl
     do
         echo "$1/$s"
     done
-    for f in $files
+    ls "$1" | while read f
     do
-	if [ $f = '.' -o $f = '..' ]
+	if [ "$f" = '.' -o "$f" = '..' ]
         then
             continue
         fi
-	namecheck=`echo $f | grep -E '^[A-za-z_]{1}[A-za-z._-]{,13}$'`
+	namecheck=`echo $f | grep -E '^[[:alpha:]_]{1}[[:alpha:]._-]{,13}$'`
 	if [ -z $namecheck ]
 	then
 	    echo "$1/$f"
-	fi
-	if [ -d "$1/$f" ]
+	elif [ -d "$1/$f" ]
         then
             findpoor "$1/$f"
 	fi 
