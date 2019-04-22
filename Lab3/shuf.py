ae7e4a4359bf557ac85194dd5801e766205f7a3c
Senyang Jiang <senyangjiang@yahoo.com>
2019-04-21 18:11:15 -0700

    lab3

--- Lab3/shuf.py
+++ Lab3/shuf.py
@@ -1,53 +1,51 @@
 #!/usr/bin/python
 
-#class shuf:
-#    def __init__(self, filename):
-#        f = open(filename, 'r')
-#        self.lines = f.readlines()
-#        f.close()
-#
-#    def permuteline(self):
-#        return random.shuffle(self.lines)
 
 import argparse, string, random, sys 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_file", nargs="*")
-    parser.add_argument("-i", "--input-range", action="store", dest="input_range")
-    parser.add_argument("-n", "--head-count", action="store", dest="head_count")
-    parser.add_argument("-r", "--repeat", action="store_true")
+    usage_msg = """./shuf.py [OPTION]... FILE
+  or:  ./shuf.py -i LO-HI [OPTION]...
 
-    options = parser.parse_args()
+Write a random permutation of the input lines to standard output.
+
+With no FILE, or when FILE is -, read standard input."""
+    
+    parser = argparse.ArgumentParser(usage=usage_msg)
+    parser.add_argument("-i", "--input-range", action="store", metavar="LO-HI", dest="input_range", help="treat each number LO through HI as an input line")
+    parser.add_argument("-n", "--head-count", action="store", metavar="COUNT", dest="head_count", help="output at most COUNT lines")
+    parser.add_argument("-r", "--repeat", action="store_true", help="output lines can be repeated")
+
+    options, args = parser.parse_known_args()
     
     if options.head_count != None:
         try:
             head_count = int(options.head_count)
         except:
-            parser.error("invalid line count: '{0}'".
-                         format(head_count))
+            sys.exit("{0}: invalid line count: '{1}'".
+                     format(sys.argv[0], options.head_count))
         if head_count < 0:
-            parser.error("invalid line count: '{0}'".
-                         format(head_count))
+            sys.exit("{0}: invalid line count: '{1}'".
+                     format(sys.argv[0], head_count))
     else:
         head_count = None
             
     if options.input_range != None:
-        if len(options.input_file) != 0:
-            parser.error("extra operand '{0}'".
-                         format(options.input_file[0]))
+        if len(args) != 0:
+            sys.exit("{0}: extra operand '{1}'".
+                     format(sys.argv[0], args[0]))
         input_range = options.input_range.split('-')
         if len(input_range) != 2:
-            parser.error("invalid input range: '{0}'".
-                         format(options.input_range))
+            sys.exit("{0}: invalid input range: '{1}'".
+                     format(sys.argv[0], options.input_range))
         try:
             LO = int(input_range[0])
             HI = int(input_range[1])
         except:
-            parser.error("invalid input range: '{0}'".
-                         format(options.input_range))
+            sys.exit("{0}: invalid input range: '{1}'".
+                     format(sys.argv[0], options.input_range))
         if LO > HI:
-            parser.error("invalid input range: '{0}'".
-                         format(options.input_range))
+            sys.exit("{0}: invalid input range: '{1}'".
+                     format(sys.argv[0], options.input_range))
         values = list(range(LO,HI+1))
         if options.repeat:
             if head_count != None:
@@ -56,6 +54,8 @@ def main():
             else:
                 while True:
                     sys.stdout.write(str(random.choice(values))+'\n')
+            return 0
+        
         numlines = HI-LO+1
         if head_count != None and head_count < numlines:
             numlines = head_count
@@ -64,15 +64,15 @@ def main():
             sys.stdout.write(str(values[i])+'\n')
         return 0
     
-    if len(options.input_file) == 0:
+    if len(args) == 0:
         input_file = sys.stdin
-    elif len(options.input_file) > 1:
-        parser.error("extra operand '{0}'".
-                     format(options.input_file[1]))
-    elif options.input_file[0] == "-":
+    elif len(args) > 1:
+        sys.exit("{0}: extra operand '{1}'".
+                 format(sys.argv[0], args[1]))
+    elif args[0] == "-":
         input_file = sys.stdin
     else:
-        input_file = options.input_file[0]
+        input_file = args[0]
 
     try:
         f = open(input_file, 'r')
@@ -80,16 +80,18 @@ def main():
         f.close()
     except IOError as e:
         errno, strerror = e.args
-        parser.error("I/O error({0}): {1}".
-                     format(errno, strerror))
+        sys.exit("{0}: {1}: {2}".
+                 format(sys.argv[0], input_file, strerror))
 
     if options.repeat:
         if head_count != None:
-            for i in range():
+            for i in range(head_count):
                 sys.stdout.write(random.choice(lines))
         else:
             while True:
                 sys.stdout.write(random.choice(lines))
+        return 0
+    
     numlines = len(lines)
     if head_count != None and head_count < numlines:
         numlines = head_count
